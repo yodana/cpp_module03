2c4bcee49278ec08254b58a996dfaf991d33570a
Yohann Dana <yodana@c3r9p5.42quebec.com>
2022-10-13 16:40:55 -0400

    ex00 done

--- ex00/srcs/ClapTrap.cpp
+++ ex00/srcs/ClapTrap.cpp
@@ -17,31 +17,45 @@ ClapTrap::ClapTrap(ClapTrap const &src){
 }
 
 ClapTrap& ClapTrap::operator=(ClapTrap const & rhs){
-    (void)rhs;
+    if (this != &rhs){
+        this->_attack = rhs._attack;
+        this->_name = rhs._name;
+        this->_health = rhs._health;
+        this->_energy = rhs._energy;
+    }
     return *this;
 }
 
 void ClapTrap::attack(const std::string &target){
-    if (this->_energy <= 0){
-        std::cout << "ClapTrap " << this->_name << "don't have energy!" << std::endl;
+    if (this->_energy <= 0 || this->_health <= 0){
+        std::cout << "ClapTrap " << this->_name << " doesn't have energy or life!" << std::endl;
         return ;
     }
-    std::cout << "ClapTrap " << this->_name << "attacks" << target << ", causing " << this->_attack << " points of damage!" << std::endl;
+    std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attack << " points of damage!" << std::endl;
     this->_energy = this->_energy - 1;
-    (void)target;
 }
 
 
 void ClapTrap::takeDamage(unsigned int amount){
     if (this->_health <= 0){
-        std::cout << "ClapTrap " << this->_name << " don't have health!" << std::endl;
+        std::cout << "ClapTrap " << this->_name << " doesn't have health!" << std::endl;
         return ;
     }
     else{
-        std::cout << "ClapTrap " << this->_name << " take " << amount << " points of damage!" << std::endl;
+        std::cout << "ClapTrap " << this->_name << " takes " << amount << " points of damage!" << std::endl;
         this->_health = this->_energy - amount;
         if (this->_health <= 0){
-            std::cout << "ClapTrap " << this->_name << " shut at 0 health!" << std::endl;
+            std::cout << "ClapTrap " << this->_name << " shuts at 0 health!" << std::endl;
         }
     }
+}
+
+void ClapTrap::beRepaired(unsigned int amount){
+    if (this->_energy <= 0 || this->_health <= 0){
+        std::cout << "ClapTrap " << this->_name << " doesn't have energy or life!" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap " << this->_name << " recovers 1 hp!" << std::endl;
+    this->_energy = this->_energy - 1;
+    this->_health = this->_health + amount;
 }
--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 
 int main( void ) {
-    ClapTrap a("moi");
-    a.attack("test");
+    ClapTrap a("yodana");
+    a.attack("the corrector");
+    for(int i = 0; i<=10;i++ )
+        a.beRepaired(1);
+    a.takeDamage(1000);
+    a.attack("the corrector");
     return 0;
 }
